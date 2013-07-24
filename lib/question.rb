500465b9f09ee35aa3aa3c7f7ff90b4fdb89e6d2
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-07-24 09:09:29 -0700

    removed unnecessary Question class

--- lib/question.rb
+++
@@ -1,9 +0,0 @@
-class Question
-  def initialize(title="", text, difficulty_level=0, category="")
-    @title = title
-    @text = text
-    @difficulty_level = difficulty_level
-    @category = category
-  end
-  attr_accessor :title, :text, :difficulty_level, :category
-end
