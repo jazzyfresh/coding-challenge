7a9f19312de4aa9045712a4235d345cce231aabd
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-07-30 13:47:29 -0700

    changed tests controller to challenges controller

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,55 @@
+class ChallengesController < ApplicationController
+
+  def index
+    @challenges = Challenge.all
+  end
+
+  def show
+    set_challenge
+  end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+  def edit
+    set_challenge
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+
+    if @challenge.save
+      redirect_to @challenge
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    set_challenge
+
+    if @challenge.update(challenge_params)
+      redirect_to @challenge
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    set_challenge
+
+    @challenge.destroy
+    redirect_to challenges_path
+  end
+
+  private
+
+    def set_challenge
+      @challenge = Challenge.find(params[:id])
+    end
+
+    def challenge_params
+      params.require(:challenge).permit(:title, :overall_difficulty, :genre)
+    end
+end
